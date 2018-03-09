6a416f6cd9396806ae25608f735057a6af815ce9
Andrielson <anfesilva@semae.riopreto.sp.gov.br>
2018-03-09 16:22:38 -0300

    Commit pra salvar os arquivos antes de mudar os valores monetários pra Integer

--- app/src/main/java/tk/andrielson/carrinhos/androidapp/ui/BindingAdapters.java
+++ app/src/main/java/tk/andrielson/carrinhos/androidapp/ui/BindingAdapters.java
@@ -3,55 +3,74 @@ package tk.andrielson.carrinhos.androidapp.ui;
 import android.databinding.BindingAdapter;
 import android.databinding.InverseBindingAdapter;
 import android.databinding.InverseBindingListener;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 import tk.andrielson.carrinhos.androidapp.textinputmoeda.CurrencyTextInputEditText;
+import tk.andrielson.carrinhos.androidapp.utils.LogUtil;
 
 /**
  * Created by anfesilva on 09/03/2018.
  */
 
 public class BindingAdapters {
+    private static final String TAG = BindingAdapters.class.getSimpleName();
+
     @BindingAdapter(value = "valorRealAttrChanged")
     public static void setListenerOnEditTextMoeda(EditText editText, final InverseBindingListener listener) {
         if (listener != null) {
-            editText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    listener.onChange();
-                }
-            });
+            editText.addTextChangedListener(new InverseBindingTextChangedListener(listener));
         }
     }
 
-    @BindingAdapter("valorReal")
-    public static void setValorReal(TextView editText, Double valor) {
-        if ((editText instanceof CurrencyTextInputEditText) && Objects.equals(valor, getValorReal((CurrencyTextInputEditText) editText)))
+    @BindingAdapter(value = "valorReal")
+    public static void setValorRealTextView(TextView textView, double valor) {
+        if (textView instanceof CurrencyTextInputEditText)
+            setValorRealCurrencyTextInputEditText((CurrencyTextInputEditText) textView, valor);
+        else
+            textView.setText(String.valueOf(valor * 10));
+    }
+
+    @BindingAdapter(value = "valorReal")
+    public static void setValorRealCurrencyTextInputEditText(CurrencyTextInputEditText editText, double valor) {
+        if (Objects.equals(valor, getValorReal(editText)))
             return;
-//        editText.setText(valor != null ? String.format(Locale.getDefault(), "%.2f", valor) : "");
         editText.setText(String.valueOf(valor * 10));
     }
 
+    @NonNull
     @InverseBindingAdapter(attribute = "valorReal")
     public static Double getValorReal(CurrencyTextInputEditText editText) {
-        /*String texto = editText.getText().toString().replace(",", ".");
-        return texto.isEmpty() ? null : Double.valueOf(texto);*/
-        return (double) (editText.getRawValue() / 100);
+        LogUtil.Log(TAG, "Dígitos decimais: " + editText.getDecimalDigits(), Log.DEBUG);
+        return ((double) editText.getRawValue()) / 100;
+    }
+
+    private static class InverseBindingTextChangedListener implements TextWatcher {
+        private final InverseBindingListener bindingListener;
+
+        InverseBindingTextChangedListener(InverseBindingListener bindingListener) {
+            this.bindingListener = bindingListener;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            bindingListener.onChange();
+        }
     }
 }
